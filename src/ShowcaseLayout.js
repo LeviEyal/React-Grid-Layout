1f47ccf404c8602783dc65d5199eb53a078a8877
Eyal Levi <48846533+LeviEyal@users.noreply.github.com>
2021-11-17 13:06:57 +0200

    Initial commit

---
+++ src/ShowcaseLayout.js
@@ -0,0 +1,133 @@
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+let count = 0;
+
+export default class ShowcaseLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kaki: false,
+      currentBreakpoint: "lg",
+      compactType: "vertical",
+      mounted: false,
+      layouts: { lg: props.initialLayout }
+    };
+
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    this.onCompactTypeChange = this.onCompactTypeChange.bind(this);
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+    this.onNewLayout = this.onNewLayout.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={""}>
+          <form className="inputText noneDraggable">
+            <label>
+              Enter your name:
+              <input type="text" />
+            </label>
+          </form>
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange(breakpoint) {
+    this.setState({
+      currentBreakpoint: breakpoint
+    });
+  }
+
+  onCompactTypeChange() {
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  }
+
+  onLayoutChange(layout, layouts) {
+    this.props.onLayoutChange(layout, layouts);
+  }
+
+  onNewLayout() {
+    count++;
+    this.setState({
+      layouts: { lg: generateLayout() }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Current Breakpoint: {this.state.currentBreakpoint} (
+          {this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <div>
+          Compaction type:{" "}
+          {_.capitalize(this.state.compactType) || "No Compaction"}
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>
+          Change Compaction Type
+        </button>
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          draggableCancel=".noneDraggable"
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+
+ShowcaseLayout.propTypes = {
+  onLayoutChange: PropTypes.func.isRequired
+};
+
+ShowcaseLayout.defaultProps = {
+  className: "layout",
+  rowHeight: 30,
+  onLayoutChange: function () {},
+  cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  initialLayout: generateLayout()
+};
+
+function generateLayout() {
+  return _.map(_.range(0, count), function (item, i) {
+    var y = 3;
+    return {
+      x: 0,
+      y: 0,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: false
+    };
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ShowcaseLayout from "./ShowcaseLayout";
+import "./styles.css";
+
+class ExampleLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { layout: [] };
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+  }
+
+  onLayoutChange(layout) {
+    this.setState({ layout: layout });
+  }
+
+  stringifyLayout() {
+    return this.state.layout.map(function (l) {
+      return (
+        <div className="layoutItem" key={l.i}>
+          <b>{l.i}</b>: [{l.x}, {l.y}, {l.w}, {l.h}]
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="layoutJSON">
+          Displayed as <code>[x, y, w, h]</code>:
+          <div className="columns">{this.stringifyLayout()}</div>
+        </div>
+        <ShowcaseLayout onLayoutChange={this.onLayoutChange} />
+      </div>
+    );
+  }
+}
+
+const contentDiv = document.getElementById("root");
+const gridProps = window.gridProps || {};
+ReactDOM.render(React.createElement(ExampleLayout, gridProps), contentDiv);
